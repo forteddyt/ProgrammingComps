d0d079a52a85df9ed860d4d64d121c1fd00fcb5b
forteddyt <forteddyt@gmail.com>
2016-09-30 10:04:30 -0400

    +Round#374 (Div 2) src Folder

---
+++ CodeForces/Round #374 (Div. 2)/ProblemA.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class ProblemA
+{
+
+    Scanner scan = new Scanner(System.in);
+
+
+    public static void main(String[] args)
+    {
+        ProblemA p = new ProblemA();
+        p.start();
+    }
+
+
+    public void start()
+    {
+
+    }
+}
