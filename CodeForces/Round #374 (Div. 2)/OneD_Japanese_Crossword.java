41a13599b3001317da59c315be522d58b9ee0acc
forteddyt <forteddyt@gmail.com>
2016-09-30 10:23:02 -0400

    +OneD_Japanese_Crossword.java Solved!

---
+++ CodeForces/Round #374 (Div. 2)/OneD_Japanese_Crossword.java
@@ -0,0 +1,58 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ * One-dimensional Japanese Crossword -
+ * http://codeforces.com/contest/721/problem/0
+ * 
+ * @author Teddy Todorov
+ * @version Sep 30, 2016
+ */
+
+public class OneD_Japanese_Crossword
+{
+
+    Scanner scan = new Scanner(System.in);
+
+
+    public static void main(String[] args)
+    {
+        OneD_Japanese_Crossword c = new OneD_Japanese_Crossword();
+        c.start();
+    }
+
+
+    public void start()
+    {
+        int n = scan.nextInt();
+        scan.nextLine();
+
+        char[] input = scan.nextLine().toCharArray();
+        int groupCounter = 0;
+        int setCounter = 0;
+
+        ArrayList<Integer> setCounterList = new ArrayList<Integer>();
+
+        for (int i = 0; i < input.length; i++)
+        {
+            char cCur = input[i];
+
+            if (cCur == 'B')
+            {
+                setCounter++;
+            }
+            if ((cCur == 'W' || i == input.length - 1) && setCounter != 0)
+            {
+                groupCounter++;
+                setCounterList.add(setCounter);
+                setCounter = 0;
+            }
+        }
+
+        System.out.println(groupCounter);
+        for (Integer i : setCounterList)
+        {
+            System.out.print(i + " ");
+        }
+    }
+}
