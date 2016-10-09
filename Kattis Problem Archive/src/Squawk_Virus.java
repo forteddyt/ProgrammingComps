74667c8d64c5d3b8362e1ed2a58dba2a315b74c2
Teddy Todorov <forteddyt@gmail.com>
2016-10-09 01:13:22 -0400

    +Squawk_Virus.java unsolved

---
+++ Kattis Problem Archive/src/Squawk_Virus.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+/**
+ * Squawk Virus - https://ecna15.kattis.com/problems/squawk
+ * 
+ * @author Teddy Todorov
+ */
+public class Squawk_Virus
+{
+    Scanner scan = new Scanner(System.in);
+
+
+    public static void main(String[] args)
+    {
+        Squawk_Virus v = new Squawk_Virus();
+        v.start();
+    }
+
+
+    public void start()
+    {
+
+    }
+}
