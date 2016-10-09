342f21dd022756937f2d71f0a1ff22451de9f3ac
Teddy Todorov <forteddyt@gmail.com>
2016-10-09 01:10:38 -0400

    +Rings.java unsolved

---
+++ Kattis Problem Archive/src/Rings.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+/**
+ * Rings - https://ecna15.kattis.com/problems/rings2
+ * 
+ * @author Teddy Todorov
+ */
+
+public class Rings
+{
+    Scanner scan = new Scanner(System.in);
+
+
+    public static void main(String[] args)
+    {
+        Rings r = new Rings();
+        r.start();
+    }
+
+
+    public void start()
+    {
+
+    }
+}
