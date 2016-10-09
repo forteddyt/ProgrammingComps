d149282aaf74a52aef9b91eb640b5e19e4e48d79
Teddy Todorov <forteddyt@gmail.com>
2016-10-09 01:12:04 -0400

    +Whats_on_the_Grille.java unsolved

---
+++ Kattis Problem Archive/src/Whats_on_the_Grille.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+/**
+ * What's on the Grille? - https://ecna15.kattis.com/problems/grille
+ * 
+ * @author Teddy Todorov
+ */
+public class Whats_on_the_Grille
+{
+
+    Scanner scan = new Scanner(System.in);
+
+
+    public static void main(String[] args)
+    {
+        Whats_on_the_Grille g = new Whats_on_the_Grille();
+        g.start();
+    }
+
+
+    public void start()
+    {
+
+    }
+
+}
