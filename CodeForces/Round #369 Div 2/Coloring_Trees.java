75d6ab50c2312dd5b7604906751585c193a35459
forteddyt <forteddyt@gmail.com>
2016-10-13 20:17:22 -0400

    +Coloring_Trees.java incomplete!

---
+++ CodeForces/Round #369 Div 2/Coloring_Trees.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.TreeMap;
+
+/**
+ * Coloring Trees - http://codeforces.com/contest/711/problem/C
+ * 
+ * I can't understand the damn paint bucket portion of it
+ * 
+ * @author Teddy
+ * @version Oct 13, 2016
+ */
+public class Coloring_Trees {
+
+    Scanner scan = new Scanner(System.in);
+
+
+    public static void main(String[] args) {
+        Coloring_Trees c = new Coloring_Trees();
+        c.start();
+    }
+
+
+    public void start() {
+        int n = scan.nextInt();
+        int k = scan.nextInt();
+        int m = scan.nextInt();
+
+    }
+}
