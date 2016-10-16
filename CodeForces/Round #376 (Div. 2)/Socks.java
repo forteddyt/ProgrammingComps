100af78e8cdab120ad6262bbd904e1c3d2535514
forteddyt <forteddyt@gmail.com>
2016-10-16 18:35:51 -0400

    Socks ~ unsolved

--- CodeForces/Round #376 (Div. 2)/Socks.java
+++ CodeForces/Round #376 (Div. 2)/Socks.java
@@ -1,9 +1,10 @@
-import java.util.Map;
+import java.util.ArrayList;
 import java.util.Scanner;
-import java.util.TreeMap;
 
 /**
  * Socks - http://codeforces.com/contest/731/problem/C
+ * 
+ * Incomplete - Graphs
  * 
  * @author Teddy
  * @version Oct 16, 2016
@@ -24,36 +25,12 @@ public class Socks {
         int m = scan.nextInt();
         int k = scan.nextInt();
 
-        Map<Integer, Integer> socks = new TreeMap<Integer, Integer>();
-        for (int i = 1; i <= n; i++) {
-            socks.put(i, (Integer)scan.nextInt());
-        }
-
-        Integer[][] days = new Integer[m][2];
-        for (int r = 0; r < m; r++) {
-            for (int c = 0; c < 2; c++) {
-                int val = scan.nextInt();
-                days[r][c] = socks.get(val);
-            }
-        }
-
-        for (int i = 0; i < m; i++) {
-            for (int q = 0; q < 2; q++) {
-                System.out.print(days[i][q] + " ");
-            }
-            System.out.println("");
-        }
-
-        socks.put(2, 2);
-        System.out.println("");
-
-        for (int i = 0; i < m; i++) {
-            for (int q = 0; q < 2; q++) {
-                System.out.print(days[i][q] + " ");
-            }
-            System.out.println("");
+        ArrayList<ArrayList<Integer>> graph =
+            new ArrayList<ArrayList<Integer>>();
+        int[] colorOptions = new int[n];
+        for (int i = 0; i < n; i++) {
+            colorOptions[i] = scan.nextInt();
         }
 
     }
-
 }
